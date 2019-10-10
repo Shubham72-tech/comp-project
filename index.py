47513bebcc67cd28244ef651143f0c424373e424
arham musheer <arhammusheer@gmail.com>
2019-10-11 03:48:03 +0530

    sql test

--- index.py
+++ index.py
@@ -1,11 +1,30 @@
 import time
 import getch
+import os
+import mysql.connector
+
+cnx = mysql.connector.connect(user='compProject', password='12345678', host='eventdips.ml', database='compProject')
+cursor = cnx.cursor()
+selectAll="SELECT * FROM `users` WHERE 1;"
 
 def mainMenu():
     print("a for all users")
     print("b for new user")
-    option=getch.getch()
+    option=getch.getche()
+    print("\n")
     return option
 
+def clearScreen():
+    if os.name == 'nt':
+        os.system('cls')  # on windows
+    else:
+        os.system('clear')  # on linux / os x
+
+def sqlCon():
+    cursor.execute(selectAll)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
 #main
-mainMenu()
+sqlCon()
+
