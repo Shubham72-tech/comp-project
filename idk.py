3ce861cb1ee2504501932e6bf4bf6f12c4f4a0ef
arhammusheer <arhammusheer@gmail.com>
2019-10-11 04:23:20 +0530

    idk

--- idk.py
+++ idk.py
@@ -1,5 +1,7 @@
 import mysql.connector
+import os
 import getch
+import time
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -15,18 +17,34 @@ def printAllUsers():
         print(x)
 
 def printUserData():
-    accNo=input("Enter Acc No")
+    accNo=input("Enter Acc No ---> ")
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM users where acc_no=%s",accNo)
+    sql = "SELECT * FROM users WHERE acc_no = %s"
+    query = (accNo,)
+    mycursor.execute(sql,query)
     myresult = mycursor.fetchall()
     for x in myresult:
         print(x)
 
 def Menu():
-    print("Print all users if a")
-    print("Print all users if b")
+    print("all users if a")
+    print("users by acc No if b")
+    print("Press q to exit")
+    print("--->")
     choice=getch.getch()
     if choice=='a':
         printAllUsers()
     elif choice=='b':
         printUserData()
+    elif choice=='q':
+        exit()
+    else:
+         print("Try Again or press q to quit ")
+         time.sleep(5)
+         if os.name == 'nt':
+             os.system('cls')
+         else:
+             os.system('clear')
+	 Menu()
+#main
+Menu()
